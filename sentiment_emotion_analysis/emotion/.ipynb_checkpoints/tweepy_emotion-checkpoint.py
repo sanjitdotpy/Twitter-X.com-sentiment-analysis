38971fd9c2939b1e7191babcdf2fa48fce1837d5
Sanjit Verma <versan@Sanjits-MacBook-Pro.local>
2023-12-15 17:27:51 -0800

    Initial commit

---
+++ sentiment_emotion_analysis/emotion/.ipynb_checkpoints/tweepy_emotion-checkpoint.py
@@ -0,0 +1,47 @@
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+from datetime import datetime, date, time, timedelta
+from collections import Counter
+import sys
+import tweepy
+import numpy as np
+import pandas as pd
+
+class Import_tweet_emotion:
+
+	consumer_key="zlvCuNmZbu0PvXJore0BViRHW"
+	consumer_secret="5TXwpPG48HssULxemgEDfxfP6v52bfNBWZGPCt8hj2dFtmLyXH"
+	access_token="849346228932497410-phFfa8XGTa3a9TQS5SZ66mBjWv298pq"
+	access_token_secret="ZrgB0r87Z1l9Nnicu01EqJEN45cMyqvlaMDCc5mSJV3Q3"
+
+	def tweet_to_data_frame(self, tweets):
+		df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+		return df
+
+	def get_tweets(self, handle):
+		auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+		auth.set_access_token(self.access_token, self.access_token_secret)
+		auth_api = API(auth)
+
+		account = handle
+		item = auth_api.user_timeline(id=account,count=20)
+		df = self.tweet_to_data_frame(item)
+
+		all_tweets = []
+		for j in range(20):
+			all_tweets.append(df.loc[j]['Tweets'])
+		return all_tweets
+
+	def get_hashtag(self, hashtag):
+		auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+		auth.set_access_token(self.access_token, self.access_token_secret)
+		auth_api = API(auth)
+
+		account = hashtag
+		all_tweets = []
+
+		for tweet in tweepy.Cursor(auth_api.search, q=account, lang='en').items(20):
+			all_tweets.append(tweet.text)
+
+		return all_tweets
